0077a86ecfd29b2b99f3ecc5c32b71a6531b549e
mssigler <mssigler@gmail.com>
2019-04-14 19:59:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,7 +33,14 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 
 
-
+// I tried a bunch of different things. I solved this lab
+// before, so I went to the bootcamp prep track and looked 
+// at the Solutions, and was surprised at the answer, and then 
+// looked at my solution to the lab and saw that it said lab
+// complete even though I didn't solve that one. 
+// I haven't been suprised by syntax for a while though, 
+// so that was actually kind of nice. 
+// but I can't in fair faith put the right answer here.
 var theFunk = funkyFunction();
 theFunk;
 theFunk();
